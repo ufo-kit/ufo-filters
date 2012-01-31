e424500c0e319f2f4bdc2da3923658d25488db07
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2012-01-31 09:30:30 +0100

    Remove dash from filename

--- src/ufo-filter-writer.c
+++ src/ufo-filter-writer.c
@@ -50,7 +50,6 @@ static gboolean filter_write_tiff(float *buffer,
     const guint32 rows_per_strip = TIFFDefaultStripSize(tif, (guint32)-1);
     TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, rows_per_strip);
 
-    /* TODO: Evaluate if this is slower than TIFFWriteEncodedStrip() */
     for (int y = 0; y < height; y++, buffer += width)
         TIFFWriteScanline(tif, buffer, y, 0);
 
@@ -58,6 +57,42 @@ static gboolean filter_write_tiff(float *buffer,
     return success;
 }
 
+static void ufo_filter_writer_process(UfoFilter *self)
+{
+    g_return_if_fail(UFO_IS_FILTER(self));
+
+    UfoFilterWriterPrivate *priv = UFO_FILTER_WRITER_GET_PRIVATE(self);
+    UfoChannel *input_channel = ufo_filter_get_input_channel(self);
+    UfoBuffer *input = ufo_channel_get_input_buffer(input_channel);
+    cl_command_queue command_queue = (cl_command_queue) ufo_filter_get_command_queue(self);
+    GString *filename = g_string_new("");
+    gint id = -1, current_frame = 0;
+
+    guint num_dims = 0;
+    guint *dim_size = NULL;
+    guint counter = 0;
+
+    while (input != NULL) {
+        ufo_buffer_get_dimensions(input, &num_dims, &dim_size);
+        g_assert(num_dims == 2);
+        const guint width = dim_size[0];
+        const guint height = dim_size[1];
+        g_object_get(input, "id", &id, NULL);
+        if (id == -1)
+            id = current_frame++;
+
+        float *data = ufo_buffer_get_host_array(input, command_queue);
+
+        g_string_printf(filename, "%s/%s%05i.tif", priv->path, priv->prefix, counter++); 
+        if (!filter_write_tiff(data, filename->str, width, height))
+            g_message("something went wrong");
+
+        ufo_channel_finalize_input_buffer(input_channel, input);
+        input = ufo_channel_get_input_buffer(input_channel);
+    }
+    g_string_free(filename, TRUE);
+    g_free(dim_size);
+}
 
 static void ufo_filter_writer_set_property(GObject *object,
     guint           property_id,
@@ -100,44 +135,6 @@ static void ufo_filter_writer_get_property(GObject *object,
             break;
     }
 }
-
-static void ufo_filter_writer_process(UfoFilter *self)
-{
-    g_return_if_fail(UFO_IS_FILTER(self));
-
-    UfoFilterWriterPrivate *priv = UFO_FILTER_WRITER_GET_PRIVATE(self);
-    UfoChannel *input_channel = ufo_filter_get_input_channel(self);
-    UfoBuffer *input = ufo_channel_get_input_buffer(input_channel);
-    cl_command_queue command_queue = (cl_command_queue) ufo_filter_get_command_queue(self);
-    GString *filename = g_string_new("");
-    gint id = -1, current_frame = 0;
-
-    guint num_dims = 0;
-    guint *dim_size = NULL;
-    guint counter = 0;
-
-    while (input != NULL) {
-        ufo_buffer_get_dimensions(input, &num_dims, &dim_size);
-        g_assert(num_dims == 2);
-        const guint width = dim_size[0];
-        const guint height = dim_size[1];
-        g_object_get(input, "id", &id, NULL);
-        if (id == -1)
-            id = current_frame++;
-
-        float *data = ufo_buffer_get_host_array(input, command_queue);
-
-        g_string_printf(filename, "%s/%s-%05i.tif", priv->path, priv->prefix, counter++); 
-        if (!filter_write_tiff(data, filename->str, width, height))
-            g_message("something went wrong");
-
-        ufo_channel_finalize_input_buffer(input_channel, input);
-        input = ufo_channel_get_input_buffer(input_channel);
-    }
-    g_string_free(filename, TRUE);
-    g_free(dim_size);
-}
-
 static void ufo_filter_writer_class_init(UfoFilterWriterClass *klass)
 {
     UfoFilterClass *filter_class = UFO_FILTER_CLASS(klass);
@@ -150,7 +147,7 @@ static void ufo_filter_writer_class_init(UfoFilterWriterClass *klass)
     reader_properties[PROP_PREFIX] = 
         g_param_spec_string("prefix",
             "Filename prefix",
-            "Prefix of input filename",
+            "Prefix of output filename",
             "",
             G_PARAM_READWRITE);
 
