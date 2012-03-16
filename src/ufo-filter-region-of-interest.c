c6982f0a117f523e41c7a6b4edd35d23f10a30d8
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2012-03-16 11:04:53 +0100

    Use g_memmove instead of loop

--- src/ufo-filter-region-of-interest.c
+++ src/ufo-filter-region-of-interest.c
@@ -83,9 +83,8 @@ static void ufo_filter_region_of_interest_process(UfoFilter *filter)
         gfloat *out_data = ufo_buffer_get_host_array(output, cmd_queue);
 
         for (guint y = 0; y < rd_height; y++) {
-            /* g_memmove(out_data + y*priv->width, in_data + (y + y1)*in_width + x1, rd_width); */
-            for (guint x = 0; x < rd_width; x++)
-                out_data[y*priv->width + x] = in_data[(y + y1)*in_width + x + x1];
+            g_memmove(out_data + y*priv->width, in_data + (y + y1)*in_width + x1, 
+                    rd_width * sizeof(gfloat));
         }
 
         ufo_channel_finalize_input_buffer(input_channel, input);
