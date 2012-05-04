fb049d6b8678f26b24d7eccb6ef7e6b0f0f10563
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-05-04 17:48:51 +0200

    Use the new processing protocol on the writer

--- src/ufo-filter-writer.c
+++ src/ufo-filter-writer.c
@@ -25,6 +25,7 @@
 struct _UfoFilterWriterPrivate {
     gchar *path;
     gchar *prefix;
+    guint counter;
 };
 
 G_DEFINE_TYPE(UfoFilterWriter, ufo_filter_writer, UFO_TYPE_FILTER)
@@ -67,46 +68,20 @@ static gboolean filter_write_tiff(float *buffer, const gchar *name,
     return success;
 }
 
-static void ufo_filter_writer_process(UfoFilter *self)
+static void ufo_filter_writer_process_cpu(UfoFilter *self, UfoBuffer *params[])
 {
-    g_return_if_fail(UFO_IS_FILTER(self));
-
     UfoFilterWriterPrivate *priv = UFO_FILTER_WRITER_GET_PRIVATE(self);
-    UfoChannel *input_channel = ufo_filter_get_input_channel(self);
-    UfoBuffer *input = ufo_channel_get_input_buffer(input_channel);
-
-    if (input == NULL)
-        return;
+    guint width, height;
+    ufo_buffer_get_2d_dimensions(params[0], &width, &height);
 
     cl_command_queue command_queue = (cl_command_queue) ufo_filter_get_command_queue(self);
-    GString *filename = g_string_new("");
-    gint id = -1, current_frame = 0;
+    float *data = ufo_buffer_get_host_array(params[0], command_queue);
+    gchar *filename = g_strdup_printf("%s/%s%05i.tif", priv->path, priv->prefix, priv->counter++);
 
-    guint num_dims = 0;
-    guint *dim_size = NULL;
-    guint counter = 0;
+    if (!filter_write_tiff(data, filename, width, height))
+        g_message("something went wrong");
 
-    while (input != NULL) {
-        ufo_buffer_get_dimensions(input, &num_dims, &dim_size);
-        g_assert(num_dims == 2);
-        const guint width = dim_size[0];
-        const guint height = dim_size[1];
-        g_object_get(input, "id", &id, NULL);
-        if (id == -1)
-            id = current_frame++;
-
-        float *data = ufo_buffer_get_host_array(input, command_queue);
-
-        g_string_printf(filename, "%s/%s%05i.tif", priv->path, priv->prefix, counter++); 
-        if (!filter_write_tiff(data, filename->str, width, height))
-            g_message("something went wrong");
-
-        ufo_channel_finalize_input_buffer(input_channel, input);
-        input = ufo_channel_get_input_buffer(input_channel);
-    }
-
-    g_string_free(filename, TRUE);
-    g_free(dim_size);
+    g_free(filename);
 }
 
 static void ufo_filter_writer_set_property(GObject *object,
@@ -157,7 +132,7 @@ static void ufo_filter_writer_class_init(UfoFilterWriterClass *klass)
 
     gobject_class->set_property = ufo_filter_writer_set_property;
     gobject_class->get_property = ufo_filter_writer_get_property;
-    filter_class->process = ufo_filter_writer_process;
+    filter_class->process_cpu = ufo_filter_writer_process_cpu;
 
     /**
      * UfoFilterWriter:prefix:
@@ -191,6 +166,7 @@ static void ufo_filter_writer_init(UfoFilterWriter *self)
     self->priv = UFO_FILTER_WRITER_GET_PRIVATE(self);
     self->priv->path = g_strdup(".");
     self->priv->prefix = NULL;
+    self->priv->counter = 0;
 
     ufo_filter_register_input(UFO_FILTER(self), "input0", 2);
 }
