c736e3027a8306f1ac40d15e66cfb82ab98de02e
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-03-26 17:55:55 +0100

    Port flat field correction filter

---
+++ src/ufo-flat-field-correction-task.c
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2011-2013 Karlsruhe Institute of Technology
+ *
+ * This file is part of Ufo.
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <math.h>
+#include "ufo-flat-field-correction-task.h"
+
+/**
+ * SECTION:ufo-flat-field-correction-task
+ * @Short_description: Write TIFF files
+ * @Title: flat_field_correction
+ *
+ */
+
+struct _UfoFlatFieldCorrectionTaskPrivate {
+    gboolean absorption_correction;
+};
+
+static void ufo_task_interface_init (UfoTaskIface *iface);
+static void ufo_cpu_task_interface_init (UfoCpuTaskIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (UfoFlatFieldCorrectionTask, ufo_flat_field_correction_task, UFO_TYPE_TASK_NODE,
+                         G_IMPLEMENT_INTERFACE (UFO_TYPE_TASK,
+                                                ufo_task_interface_init)
+                         G_IMPLEMENT_INTERFACE (UFO_TYPE_CPU_TASK,
+                                                ufo_cpu_task_interface_init))
+
+#define UFO_FLAT_FIELD_CORRECTION_TASK_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), UFO_TYPE_FLAT_FIELD_CORRECTION_TASK, UfoFlatFieldCorrectionTaskPrivate))
+
+enum {
+    PROP_0,
+    PROP_ABSORPTION_CORRECTION,
+    N_PROPERTIES
+};
+
+static GParamSpec *properties[N_PROPERTIES] = { NULL, };
+
+UfoNode *
+ufo_flat_field_correction_task_new (void)
+{
+    return UFO_NODE (g_object_new (UFO_TYPE_FLAT_FIELD_CORRECTION_TASK, NULL));
+}
+
+static void
+ufo_flat_field_correction_task_setup (UfoTask *task,
+                                      UfoResources *resources,
+                                      GError **error)
+{
+}
+
+static void
+ufo_flat_field_correction_task_get_requisition (UfoTask *task,
+                                                UfoBuffer **inputs,
+                                                UfoRequisition *requisition)
+{
+    ufo_buffer_get_requisition (inputs[0], requisition);
+}
+
+static void
+ufo_flat_field_correction_task_get_structure (UfoTask *task,
+                                              guint *n_inputs,
+                                              UfoInputParam **in_params,
+                                              UfoTaskMode *mode)
+{
+    *mode = UFO_TASK_MODE_SINGLE;
+    *n_inputs = 3;
+    *in_params = g_new0 (UfoInputParam, 3);
+    (*in_params)[0].n_dims = 2;
+    (*in_params)[1].n_dims = 2;
+    (*in_params)[2].n_dims = 2;
+}
+
+static gboolean
+ufo_flat_field_correction_task_process (UfoCpuTask *task,
+                                        UfoBuffer **inputs,
+                                        UfoBuffer *output,
+                                        UfoRequisition *requisition)
+{
+    UfoFlatFieldCorrectionTaskPrivate *priv;
+    gfloat *proj_data;
+    gfloat *dark_data;
+    gfloat *flat_data;
+    gfloat *out_data;
+    gsize n_pixels;
+
+    priv = UFO_FLAT_FIELD_CORRECTION_TASK_GET_PRIVATE (task);
+
+    proj_data = ufo_buffer_get_host_array (inputs[0], NULL);
+    dark_data = ufo_buffer_get_host_array (inputs[1], NULL);
+    flat_data = ufo_buffer_get_host_array (inputs[2], NULL);
+    out_data = ufo_buffer_get_host_array (output, NULL);
+    n_pixels = requisition->dims[0] * requisition->dims[1];
+
+    if (priv->absorption_correction) {
+        for (gsize i = 0; i < n_pixels; i++)
+            out_data[i] = (gfloat) - log ((proj_data[i] - dark_data[i]) / (flat_data[i] - dark_data[i]));
+    }
+    else {
+        for (gsize i = 0; i < n_pixels; i++)
+            out_data[i] = (proj_data[i] - dark_data[i]) / (flat_data[i] - dark_data[i]);
+    }
+    return TRUE;
+}
+
+static void
+ufo_flat_field_correction_task_set_property (GObject *object,
+                                             guint property_id,
+                                             const GValue *value,
+                                             GParamSpec *pspec)
+{
+    UfoFlatFieldCorrectionTaskPrivate *priv = UFO_FLAT_FIELD_CORRECTION_TASK_GET_PRIVATE (object);
+
+    switch (property_id) {
+        case PROP_ABSORPTION_CORRECTION:
+            priv->absorption_correction = g_value_get_boolean (value);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+            break;
+    }
+}
+
+static void
+ufo_flat_field_correction_task_get_property (GObject *object,
+                                             guint property_id,
+                                             GValue *value,
+                                             GParamSpec *pspec)
+{
+    UfoFlatFieldCorrectionTaskPrivate *priv = UFO_FLAT_FIELD_CORRECTION_TASK_GET_PRIVATE (object);
+
+    switch (property_id) {
+        case PROP_ABSORPTION_CORRECTION:
+            g_value_set_boolean (value, priv->absorption_correction);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+            break;
+    }
+}
+
+static void
+ufo_flat_field_correction_task_finalize (GObject *object)
+{
+    G_OBJECT_CLASS (ufo_flat_field_correction_task_parent_class)->finalize (object);
+}
+
+static void
+ufo_task_interface_init (UfoTaskIface *iface)
+{
+    iface->setup = ufo_flat_field_correction_task_setup;
+    iface->get_structure = ufo_flat_field_correction_task_get_structure;
+    iface->get_requisition = ufo_flat_field_correction_task_get_requisition;
+}
+
+static void
+ufo_cpu_task_interface_init (UfoCpuTaskIface *iface)
+{
+    iface->process = ufo_flat_field_correction_task_process;
+}
+
+static void
+ufo_flat_field_correction_task_class_init (UfoFlatFieldCorrectionTaskClass *klass)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+    gobject_class->set_property = ufo_flat_field_correction_task_set_property;
+    gobject_class->get_property = ufo_flat_field_correction_task_get_property;
+    gobject_class->finalize = ufo_flat_field_correction_task_finalize;
+
+    properties[PROP_ABSORPTION_CORRECTION] =
+        g_param_spec_boolean("absorption-correction",
+                             "Take the negative natural logarithm of the result",
+                             "Take the negative natural logarithm of the result",
+                             FALSE,
+                             G_PARAM_READWRITE);
+
+    for (guint i = PROP_0 + 1; i < N_PROPERTIES; i++)
+        g_object_class_install_property (gobject_class, i, properties[i]);
+
+    g_type_class_add_private (gobject_class, sizeof(UfoFlatFieldCorrectionTaskPrivate));
+}
+
+static void
+ufo_flat_field_correction_task_init(UfoFlatFieldCorrectionTask *self)
+{
+    self->priv = UFO_FLAT_FIELD_CORRECTION_TASK_GET_PRIVATE(self);
+    self->priv->absorption_correction = FALSE;
+}
