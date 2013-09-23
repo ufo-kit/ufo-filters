ea1fc3f8f918126bcefa3d96ce53562c0361fec9
Timo Dörr <timo@latecrew.de>
2013-09-23 15:36:36 +0200

    Add C-based integration tests

---
+++ tests/integration_tests/run_local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# use the filters in this project, not the system-wide intalled ones
+UFO_PLUGIN_PATH="`pwd`/../../src/" ./test-suite
---
+++ tests/integration_tests/test-suite.h
@@ -0,0 +1,7 @@
+#ifndef TEST_SUITE_H
+#define TEST_SUITE_H
+
+void test_add_complete_local_setup (void);
+void test_add_complete_remote_setup (void);
+
+#endif
