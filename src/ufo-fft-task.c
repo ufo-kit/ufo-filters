02b0b20318b3501ee7abf14447748f5015efd965
Roman Shkarin <mathii2009@gmail.com>
2014-08-15 12:51:41 +0200

    Fix the number of event in queue so that FFT will be executed just after fft_spread kernel.

--- src/ufo-fft-task.c
+++ src/ufo-fft-task.c
@@ -288,7 +288,7 @@ ufo_fft_task_process (UfoTask *task,
     clfftEnqueueTransform (priv->fft_plan, 
                            CLFFT_FORWARD, 1, &(priv->cmd_queue),
                            (priv->auto_zeropadding)? 1 : 0,
-		           (priv->auto_zeropadding)? &event : NULL, NULL, 
+			    (priv->auto_zeropadding)? &event : NULL, NULL, 
                            (priv->auto_zeropadding)? &out_mem : &in_mem, &out_mem, NULL);
     #else
     clFFT_ExecuteInterleaved_Ufo (priv->cmd_queue, priv->fft_plan,
