1caf0ab48e3843813d9cdaa334c3ede5500710ea
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2016-11-13 19:05:44 -0800

    Remove impossible comparison

--- src/readers/ufo-edf-reader.c
+++ src/readers/ufo-edf-reader.c
@@ -120,9 +120,6 @@ ufo_edf_reader_read (UfoReader *reader,
         /* Read the full ROI at once if no stepping is specified */
         num_bytes = width * roi_height;
         num_read = fread (data, 1, num_bytes, priv->fp);
-
-        if (num_bytes != num_bytes)
-            return;
     }
     else {
         for (guint i = 0; i < num_rows - 1; i++) {
