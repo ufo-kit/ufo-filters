b4a28118497b05e3368667863f24146fd4bf3146
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2017-10-13 11:43:53 +0200

    Add tests and execute them with TravisCI

---
+++ tests/test-file-write-regression.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ufo-launch dummy-data width=512 height=512 number=3 ! write filename=foo-%02i.tif
+
+files=("*.tif")
+files=$(echo ${files[*]})
+
+# cleanup
+rm -f foo-*.tif
+
+[ "$files" == "foo-00.tif foo-01.tif foo-02.tif" ] && exit 0 || exit 1
